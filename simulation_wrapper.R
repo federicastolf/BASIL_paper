4f5bf30158a0a36a4c0b86144961bc0977aee445
federicastolf <federica.stolf@gmail.com>
2025-07-28 16:10:11 -0400

    updated sim

--- simulation_wrapper.R
+++ simulation_wrapper.R
@@ -1,6 +1,7 @@
 library(msigdbr)
 library(tidyverse)
-library(FABLE)
+
+source("FACTOR_CODE_update.R")
 
 get_geneSetMatrix = function(p, q){
   # msig_data = msigdbr(species = "Homo sapiens", category = "H")
@@ -21,7 +22,7 @@ get_geneSetMatrix = function(p, q){
 }
 
 
-syntheticData = function(n, p, k, q, sigma_sq_0, sd_gamma, sd_psi, mseed, C_mispeficied=F){
+syntheticData = function(n, p, k, q, sigma_sq_0, sd_gamma, sd_psi, mseed){
   
   set.seed(mseed)
   C = get_geneSetMatrix(p, q)
@@ -51,135 +52,7 @@ syntheticData = function(n, p, k, q, sigma_sq_0, sd_gamma, sd_psi, mseed, C_misp
   Y = M_0 %*% t(Lambda_0) + sqrt(sigma_sq_0) * matrix(rnorm(n*p), nrow=n) 
   colnames(Y) = rownames(C)
   
-  if(C_mispeficied==T){
-    # set to zero the 20% of ones randomly selected
-    Cmisp = C
-    i1 = which(Cmisp==1)
-    idx0 = sample(x = i1, size = 0.5*length(i1), replace=F)
-    Cmisp[idx0] = 0
-    if(sum(colSums(Cmisp)==0)>0){
-      Cmisp = Cmisp[, -which(colSums(Cmisp)==0)]
-    }
-    return(list("Y" = Y, "Lambda0_outer" = Lambda0_outer, "C" = C, "Cmisp" = Cmisp))
-  }
-  else{
-    return(list("Y" = Y, "Lambda0_outer" = Lambda0_outer, "C" = C))
-  }
+  return(list("Y" = Y, "Lambda0_outer" = Lambda0_outer, "C" = C))
 }
 
 
-#----------------------------------# FABLE #----------------------------------#
-# slight modification of code from https://github.com/shounakch/FABLE/tree/main
-# to implement the FABLE methodology from
-# Shounak Chattopadhyay, Anru R. Zhang, and David B. Dunson, (2024) 
-# "Blessing of dimension in Bayesian inference on covariance matrices"
-# arXiv preprint arXiv:2404.03805
-
-
-PseudoPosteriorMean_2 <- function(Y,
-                                  gamma0 = 1,
-                                  delta0sq = 1,
-                                  maxProp = 0.5) {
-  
-  tFABLEPostMean1 = proc.time()
-  
-  Y = as.matrix(Y)
-  n = nrow(Y)
-  p = ncol(Y)
-  svdY = svd(Y)
-  U_Y = svdY$u
-  V_Y = svdY$v
-  svalsY = svdY$d
-  kMax = min(which(cumsum(svalsY) / sum(svalsY) >= maxProp))
-  
-  kEst = RankEstimator(Y, 
-                       U_Y,
-                       V_Y,
-                       svalsY,
-                       kMax)
-  
-  FABLEHypPars = FABLEHyperParameters(Y,
-                                      U_Y,
-                                      V_Y,
-                                      svalsY,
-                                      kEst,
-                                      gamma0,
-                                      delta0sq)
-  
-  Part1 = FABLEHypPars$G
-  Part2 = as.numeric(FABLEHypPars$gammaDeltasq / (FABLEHypPars$gamman - 2))
-  CovEst = Part1 + diag(Part2)
-  
-  tFABLEPostMean2 = proc.time()
-  tPostMean = (tFABLEPostMean2 - tFABLEPostMean1)[3]
-  
-  OutputList = list("FABLEPostMean" = CovEst,
-                    "Lambda_outer" = Part1,
-                    "FABLEHyperParameters" = FABLEHypPars,
-                    "svdY" = svdY,
-                    "estRank" = kEst,
-                    "runTime" = tPostMean)
-  
-  return(OutputList)
-  
-}
-
-
-PseudoPosteriorSampler_2 <- function(fit,
-                                     Y,
-                                     gamma0 = 1,
-                                     delta0sq = 1,
-                                     maxProp = 0.5,
-                                     MC = 1000) {
-  
-  tFABLESample1 = proc.time()
-  
-  Y = as.matrix(Y)
-  n = nrow(Y)
-  p = ncol(Y)
-  svdY = svd(Y)
-  U_Y = svdY$u
-  V_Y = svdY$v
-  svalsY = svdY$d
-  #kMax = min(which(cumsum(svalsY) / sum(svalsY) >= maxProp))
-  kEst = fit$estRank
-  
-  FABLEHypPars = FABLEHyperParameters(Y,
-                                      U_Y,
-                                      V_Y,
-                                      svalsY,
-                                      kEst,
-                                      gamma0,
-                                      delta0sq)
-  
-  CovCorrectMatrix = cov_correct_matrix(FABLEHypPars$SigmaSqEstimate, 
-                                        FABLEHypPars$G)
-  
-  varInflation = (sum(CovCorrectMatrix) / (p*(p+1)/2))^2
-  
-  FABLESamples = FABLESampler(Y, 
-                              gamma0, 
-                              delta0sq, 
-                              MC,
-                              U_Y,
-                              V_Y,
-                              svalsY,
-                              kEst,
-                              FABLEHypPars$tauSqEstimate,
-                              FABLEHypPars$gammaDeltasq,
-                              FABLEHypPars$G0,
-                              varInflation)
-  
-  tFABLESample2 = proc.time()
-  tSample = (tFABLESample2 - tFABLESample1)[3]
-  
-  OutputList = list("CCFABLESamples" = FABLESamples,
-                    "FABLEHyperParameters" = FABLEHypPars,
-                    "svdY" = svdY,
-                    "estRank" = kEst,
-                    "varInflation" = varInflation,
-                    "runTime" = tSample)
-  
-  return(OutputList)
-  
-}
--- simulations.R
+++ simulations.R
@@ -4,21 +4,22 @@ rm(list=ls())
 
 source('helper_functions.R')
 source("simulation_wrapper.R")
-source("FACTOR_ANALYSIS/FACTOR_CODE_update.R")
 
 # set parameters
 Nsim = 25
-param = list(n = 500, p = 1000, k = 10, q = 500, sigma_sq_0 = 0.5,
-             sd_gamma = 2, sd_psi = 0.5)
-# param = list(n = 500, p = 3000, k = 10, q = 500, sigma_sq_0 = 0.5,
-#              sd_gamma = 1, sd_psi = 0.3)
+# high biological signal
+# param = list(n = 500, p = 1000, k = 10, q = 500, sigma_sq_0 = 15, sd_gamma = 0.75,
+#              sd_psi = 0.1)
+
+#low biological signal
+param = list(n = 500, p = 3000, k = 10, q = 500, sigma_sq_0 = 15, sd_gamma = 0.4,
+             sd_psi = 0.75)
 
 
 set.seed(463)
 seeds_g = sample.int(9000, Nsim)
 
 kfitBASIL = err_normBASIL = timeBASIL = rep(0, Nsim)
-# kfitFABLE = err_normFABLE  = timeFABLE = rep(0, Nsim)
 kfitROTATE = err_normROTATE  = timeROTATE = rep(0, Nsim)
 kfitPLIER = err_normPLIER  = timePLIER = rep(0, Nsim)
 data_all = vector("list", Nsim)
@@ -27,7 +28,7 @@ for(s in 1:Nsim){
   #simulate data
   datas = syntheticData(n = param$n, p = param$p, k = param$k, q = param$q, 
                         sigma_sq_0 = param$sigma_sq_0, sd_gamma = param$sd_gamma,
-                        sd_psi = param$sd_psi, mseed = seeds_g[s], C_mispeficied = F)
+                        sd_psi = param$sd_psi, mseed = seeds_g[s])
   data_all[[s]] = datas
   Ys = datas$Y
   Cs = datas$C
@@ -44,9 +45,8 @@ for(s in 1:Nsim){
     norm(Lambda0_outer, type='F')
   kfitBASIL[s] = est_kBASIL$k_hat
 
-  # compute rotate
-
-  K = 30
+  # compute ROTATE
+  K = param$k
   startB = matrix(rnorm(param$p*K),param$p,K)
   start = list(B = startB, sigma = rep(1,K), theta = rep(0.5,K))
   ptmR = proc.time()
@@ -59,48 +59,38 @@ for(s in 1:Nsim){
   err_normROTATE[s] = norm(tcrossprod(Lambda_ROTATE) - Lambda0_outer, type='F')/
     norm(Lambda0_outer, type='F')
   
-  # # compute FABLE
-  # ptmF = proc.time()
-  # fitFABLE = PseudoPosteriorMean_2(Ys)
-  # etmF = proc.time() - ptmF
-  # timeFABLE[s] = etmF[1] + etmF[2]
-  # kfitFABLE[s] = fitFABLE$estRank
-  # err_normFABLE[s] = norm(fitFABLE$Lambda_outer - Lambda0_outer, type='F')/
-  #   norm(Lambda0_outer, type='F')
-  # 
-  # # compute PLIER
-  # ptmP = proc.time()
-  # fitPLIER = PLIER(t(Ys), Cs, scale = F, minGenes = 1, doCrossval = T)
-  # etmP = proc.time() - ptmP
-  # timePLIER[s] = etmP[1] + etmP[2]
-  # kfitPLIER[s] = nrow(fitPLIER$B)
-  # covPLIER = (fitPLIER$Z %*% (fitPLIER$B %*% t(fitPLIER$B)) %*% t(fitPLIER$Z))/param$n
-  # err_normPLIER[s] = norm(covPLIER - Lambda0_outer, type='F')/
-  #   norm(Lambda0_outer, type='F')
-  
+  # compute PLIER
+  ptmP = proc.time()
+  fitPLIER = PLIER(t(Ys), Cs, scale = F, minGenes = 1, doCrossval = T)
+  etmP = proc.time() - ptmP
+  timePLIER[s] = etmP[1] + etmP[2]
+  kfitPLIER[s] = nrow(fitPLIER$B)
+  covPLIER = (fitPLIER$Z %*% (fitPLIER$B %*% t(fitPLIER$B)) %*% t(fitPLIER$Z))/param$n
+  err_normPLIER[s] = norm(covPLIER - Lambda0_outer, type='F')/
+    norm(Lambda0_outer, type='F')
+
 }
 
 summary(err_normROTATE)
 summary(err_normBASIL)
+summary(err_normPLIER)
 
-# c(median(kfitBASIL), IQR(kfitBASIL))
-# c(median(kfitFABLE), IQR(kfitFABLE))
-# c(median(err_normBASIL), IQR(err_normBASIL))
-# c(median(err_normROTATE), IQR(err_normBASIL))
-# c(median(err_normFABLE), IQR(err_normFABLE))
-# c(median(timeBASIL), IQR(timeBASIL))
-# c(median(timeFABLE), IQR(timeFABLE))
-# 
-# c(median(kfitPLIER), IQR(kfitPLIER))
-# c(median(err_normPLIER), IQR(err_normPLIER))
-# c(median(timePLIER), IQR(timePLIER))
-# 
-# SimRes1 = cbind.data.frame(c(err_normBASIL, err_normFABLE, err_normPLIER), 
-#                           c(timeBASIL, timeFABLE, timePLIER),
-#                           c(kfitBASIL, kfitFABLE, kfitPLIER),
-#                           c(rep("BASIL",Nsim), rep("FABLE",Nsim),  rep("PLIER",Nsim)),
+
+# SimResLow1 = cbind.data.frame(c(err_normBASIL, err_normROTATE, err_normPLIER),
+#                           c(timeBASIL, timeROTATE, timePLIER),
+#                           c(kfitBASIL, kfitROTATE, kfitPLIER),
+#                           c(rep("BASIL",Nsim), rep("ROTATE",Nsim),  rep("PLIER",Nsim)),
 #                           rep("3000", Nsim*3))
-# colnames(SimRes1) = c("err_norm", "time", "k_est", "model", "p")
+# colnames(SimResLow1) = c("err_norm", "time", "k_est", "model", "p")
+# # 
 # 
-# # save(SimRes, file="simResults.Rdata")
+# load("simResultsLow.Rdata")
+# SimResLow = rbind.data.frame(SimResLow, SimResLow1)
+# save(SimResLow, file="simResultsLow.Rdata")
+# 
+
+# B1 = SimResLow %>% filter(p=="3000") %>% filter(model=="PLIER") 
+# c(median(B1$err_norm), IQR(B1$err_norm))
+# c(median(B1$k_est), IQR(B1$k_est))
+# c(median(B1$time), IQR(B1$time))
 
