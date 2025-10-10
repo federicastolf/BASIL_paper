5b66fe6d7924a4b2fd0498e50fee8da3216a2c4e
federicastolf <federica.stolf@gmail.com>
2025-10-10 14:34:29 -0400

    update WB

--- BloodAnalysis.R
+++ BloodAnalysis.R
@@ -5,10 +5,18 @@ library(gridExtra)
 library(pheatmap)
 library(scico)
 library(mvtnorm)
+library(circlize)
+library(ComplexHeatmap)
+library(ggraph)
+library(igraph)
+library(RColorBrewer)
+
 rm(list=ls())
 
-#---------------------------# human whole blood data #------------------------#
+source('helper_functions.R')
+source('plot_functions.R')
 
+# load data
 data("dataWholeBlood") 
 data("bloodCellMarkersIRISDMAP")
 data("canonicalPathways")
@@ -24,8 +32,6 @@ p = ncol(Y)
 # filter 100 genes with largest variance
 gene_variances <- apply(t(dataWholeBlood), 2, var)
 id <- order(gene_variances, decreasing = TRUE)[1:100]
-dataWholeBlood_top100 <- t(dataWholeBlood)[,id]
-
 
 #----# fit PLIER #-----#
 PLresult = PLIER(t(Y), allPaths, k = 8, doCrossval = F, scale = F)
@@ -33,7 +39,6 @@ covPLIER = PLresult$Z %*% (cov(t(PLresult$B))) %*% t(PLresult$Z)
 corrPLIER = cov2cor(covPLIER)
 
 #----# fit BASIL #-----#
-source('helper_functions.R')
 svd_Y = svd(Y)
 k_max = min(which((cumsum(svd_Y$d^2) / sum(svd_Y$d^2) > 0.8)))
 k_max
@@ -45,6 +50,7 @@ Lambda_hatWB = fitWB$Lambda_C + fitWB$Lambda_N
 CovBasilWB = Lambda_hatWB %*% t(Lambda_hatWB) + fitWB$sigma_sq*diag(ncol(Y))
 corrBasilWb = cov2cor(CovBasilWB)
 
+# UQ for BASIL
 basil_loadings_samples = compute_posterior_samples_cc(
   Y, fitWB$Lambda_C, fitWB$Lambda_N, fitWB$tau_C, fitWB$tau_N,
   fitWB$sigma_sq, fitWB$P_C, v0=1, sigma_sq_0=1, n_MC=500
@@ -60,7 +66,7 @@ corrBasil_qs = apply(cor_est$posterior_samples, c(1,2), function(x)(quantile(x, 
 corrBasil_mean_zeroed[((corrBasil_qs[1,,] <0) & (corrBasil_qs[2,,] >0))] = 0
 mean(corrBasil_mean_zeroed==0)
 
-#------------------------# correlation Plot #---------------------#
+#---------------------------------# correlation Plot #-------------------------#
 
 # empirical correlation 
 sub_data <- Y[,id]
@@ -69,99 +75,34 @@ corP = cor(sub_data)
 # color blind palette
 cols = scico(51, palette = "vik") 
 breaks = seq(-1, 1, length.out = 52)
-EmpPlot = pheatmap(corP, treeheight_row = 0, treeheight_col = 0, cluster_rows = T,
-                   cluster_cols = T, border_color ="NA", legend=F, show_colnames = F,
-                   show_rownames = F, breaks = breaks, color = cols, fontsize = 12,
-                   main = "Empirical")
 
-row_order = EmpPlot$tree_row$order
-col_order = EmpPlot$tree_col$order
+# empirical correlation
+emp_corr_plot = plot_correlation_heatmap(corP, title = "Empirical", cluster = TRUE)
+row_order = emp_corr_plot$tree_row$order
+col_order = emp_corr_plot$tree_col$order
 
+# BASIL
 row.names(corrBasil_mean) = colnames(sub_data)
 colnames(corrBasil_mean) = colnames(sub_data)
+basil_corr_plot = plot_correlation_heatmap(corrBasil_mean, title = "BASIL", 
+                                           row_order = row_order, col_order = col_order)
 
-
-BSPlot = pheatmap(corrBasil_mean[row_order, col_order], treeheight_row = 0, treeheight_col = 0,
-                  cluster_rows = F, cluster_cols = F, border_color ="NA", legend=F,
-                  show_colnames = F, show_rownames = F, breaks = breaks, color = cols, 
-                  fontsize = 12, main = "BASIL")
-
+# BASIL UQ
 row.names(corrBasil_mean_zeroed) = colnames(sub_data)
 colnames(corrBasil_mean_zeroed) = colnames(sub_data)
+basilUQ_corr_plot = plot_correlation_heatmap(
+  corrBasil_mean_zeroed, title = "BASIL-UQ", row_order = row_order, col_order = col_order)
 
-BSPlot_uq = pheatmap(corrBasil_mean_zeroed[row_order, col_order], treeheight_row = 0, treeheight_col = 0,
-                     cluster_rows = F, cluster_cols = F, border_color ="NA", legend=F,
-                     show_colnames = F, show_rownames = F, breaks = breaks, color = cols, 
-                     fontsize = 12, main = "BASIL-UQ")
-
-
+#PLIER
 sub_corrPLIER = corrPLIER[id,id]
-PLPlot = pheatmap(sub_corrPLIER[row_order, col_order], treeheight_row = 0, treeheight_col = 0, 
-                  cluster_rows = F, cluster_cols = F, border_color ="NA", legend=F, 
-                  show_colnames = F, show_rownames = F, breaks = breaks, color = cols, 
-                  fontsize = 12, main="PLIER")
+plier_corr_plot = plot_correlation_heatmap(sub_corrPLIER, title = "PLIER",
+                                           row_order = row_order, col_order = col_order)
 
-grid.arrange(EmpPlot$gtable, BSPlot$gtable, BSPlot_uq$gtable,  PLPlot$gtable, ncol = 4)
-CorrNP = grid.arrange(EmpPlot$gtable, BSPlot$gtable, BSPlot_uq$gtable,  PLPlot$gtable, ncol = 4)
+corrWB = grid.arrange(emp_corr_plot$gtable, basil_corr_plot$gtable, basilUQ_corr_plot$gtable,
+                               plier_corr_plot$gtable, ncol = 4)
 # ggsave('CorrWholeBLOOD1.png', plot=CorrNP, device = 'png', width = 9.1, height = 3.3)
 
-
-#-------------------# observed vs predicted plot #-------------------------#
-
-
-df_basil = data.frame(Observed = corP[lower.tri(corP)], 
-                      Predicted = corrBasil_mean[lower.tri(corrBasil_mean)], 
-                      Method = "BASIL")
-df_plier = data.frame(Observed = corP[lower.tri(corP)], 
-                      Predicted = sub_corrPLIER[lower.tri(sub_corrPLIER)],
-                      Method = "PLIER")
-lims = range(c(df_basil$Observed, df_basil$Predicted, 
-               df_plier$Observed, df_plier$Predicted))
-
-# library(scales)
-# library(ggthemes)
-# show_col(tableau_color_pal("Color Blind")(10))
-
-# BASIL plot
-p1 = ggplot(df_basil, aes(x = Observed, y = Predicted)) +
-  geom_point(color = "#1170aa", shape = 1) +
-  geom_abline(intercept = 0, slope = 1, color = "black", linewidth = 1) +
-  labs(title = "BASIL", x = "Observed", y = "Predicted") +
-  xlim(lims) + ylim(lims) +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size=20),
-        axis.title=element_text(size=16))
-p1
-
-# PLIER plot
-p2 = ggplot(df_plier, aes(x = Observed, y = Predicted)) +
-  geom_point(color = "#1170aa", shape = 1) +
-  geom_abline(intercept = 0, slope = 1, color = "black", linewidth = 1) +
-  labs(title = "PLIER", x = "Observed", y = "Predicted") +
-  xlim(lims) + ylim(lims) +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size=20),
-        axis.title=element_text(size=16),)
-p2
-
-obspPLot = grid.arrange(p1,p2, nrow=1)
-
-dev.new()
-obspPLot
-
-# ggsave('ObsPWholeBLOOD.png', plot=obspPLot, device = 'png', width = 8, height = 4)
-
-
-#-------------------# gene network plot #-------------------------#
-
-library(circlize)
-library(ComplexHeatmap)
-library(dplyr)
-library(ggraph)
-library(igraph)
-library(RColorBrewer)
-library(reshape2)
-
+#----------------------------# gene network plot #-----------------------------#
 
 df_long <- melt(corrBasil_mean[]) %>%
   filter(value < 0.9999)
@@ -170,9 +111,10 @@ df_sorted <- df_long %>%
 head(df_sorted, n=20)
 
 
-dev.new()
 network_plot <- plot_gene_network(corrBasil_mean_zeroed, n = 50)
-ggsave('network_plot_WholeBLOOD.png', plot=network_plot, device = 'png', width = 8, height = 4)
+network_plot
+# ggsave('network_plot_WholeBLOOD.png', plot=network_plot, device = 'png', 
+#        width = 8, height = 4)
 
 
 
