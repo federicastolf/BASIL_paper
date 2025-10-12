d815b504db2e66b2b5e6b5a8acb53cf160399fdf
federicastolf <federica.stolf@gmail.com>
2025-10-12 16:14:23 -0400

    corr plot

--- Fever_analysis.R
+++ Fever_analysis.R
@@ -1,46 +1,76 @@
 library(tidyverse)
 library(scico)
 library(gridExtra)
+library(pheatmap)
+library(PLIER)
 
 rm(list=ls())
 
 load("data_fever/data_Gfever.Rdata")
 source('helper_functions.R')
+#load("data_fever/fever_covariate.Rdata")
+source("plot_functions.R")
 
 Y = t(data)
 Y = scale(Y)
 
+#-----------------# estimate BASIL #----------------------#
 est_k = estimate_latent_dimension(Y, k_max=50)
 fitGF = compute_point_estimates(Y,  geneSetMat, k=est_k$k_hat)
 fitGF$tau_C/fitGF$tau_N
-
 Lambda_hat = fitGF$Lambda_C + fitGF$Lambda_N
-CovBasil = Lambda_hat %*% t(Lambda_hat) + fitGF$sigma_sq*diag(ncol(Y))
-corrBasil = cov2cor(CovBasil)
 
-#----------------# check grafico correlation #--------------------#
-# random select 50 genes
-set.seed(139)
-id = sample(c(1:NCOL(Y)), 50, replace = F)
-sub_data = Y[,id]
+#------------------------------------------------------------------------------#
+#----------------------# correlation plot (Fig. 1) #---------------------------#
+
+# selet the 100 genes with largest variance
+gene_variances <- apply(Y, 2, var)
+id <- order(gene_variances, decreasing = TRUE)[1:100]
+sub_data <- Y[,id]
 
 # empirical correlation 
 corP = cor(sub_data)
 
-# color blind palette
-cols = scico(51, palette = "vik") 
+# BASIl correlation
+CovBasil = Lambda_hat %*% t(Lambda_hat) + fitGF$sigma_sq*diag(ncol(Y))
+CovBasil_sub = CovBasil[id,id]
+corrBasil = cov2cor(CovBasil_sub)
+
+# fit PLIER
+PLresult = PLIER(t(Y), geneSetMat, k = est_k$k_hat, doCrossval = F, scale = F)
+covPLIER = PLresult$Z %*% (cov(t(PLresult$B))) %*% t(PLresult$Z)
+CovPLIER_sub = covPLIER[id,id]
+corrPLIER = cov2cor(CovPLIER_sub)
+
+#--------# correlation plot #-------#
+cols = scico(51, palette = "vik") # color blind friendly palette
 breaks = seq(-1, 1, length.out = 52)
-EmpPlot = pheatmap(corP, treeheight_row = 0, treeheight_col = 0, cluster_rows = T,
-                   cluster_cols = T, border_color ="NA", legend=F, show_colnames = F,
-                   show_rownames = F, breaks = breaks, color = cols, fontsize = 12,
-                   main = "Empirical")
-row_order = EmpPlot$tree_row$order
-col_order = EmpPlot$tree_col$order
 
-sub_corrBasil = corrBasil[id,id]
-BSPlot = pheatmap(sub_corrBasil[row_order, col_order], treeheight_row = 0, treeheight_col = 0,
-                  cluster_rows = F, cluster_cols = F, border_color ="NA", legend=F,
-                  show_colnames = F, show_rownames = F, breaks = breaks, color = cols, 
-                  fontsize = 12, main = "BASIL")
+emp_corr_plot = plot_correlation_heatmap(corP, title = "Empirical", cluster = TRUE)
+row_order = emp_corr_plot$tree_row$order
+col_order = emp_corr_plot$tree_col$order
+basil_corr_plot = plot_correlation_heatmap(corrBasil, title = "BASIL", 
+                                           row_order = row_order, col_order = col_order)
+plier_corr_plot = plot_correlation_heatmap(corrPLIER, title = "PLIER",
+                                           row_order = row_order, col_order = col_order)
+corrNP = grid.arrange(emp_corr_plot$gtable, basil_corr_plot$gtable, 
+                               plier_corr_plot$gtable, ncol = 3)
+# ggsave('CorrFever.png', plot=CorrNP, device = 'png', width = 9.1, height = 3.3)
 
-CorrNP = grid.arrange(EmpPlot$gtable, BSPlot$gtable,  ncol = 2)
+#------# observed vs predicted plot #--------#
+
+df_basil_cplot = data.frame(Observed = corP[lower.tri(corP)], 
+                            Predicted = corrBasil[lower.tri(corrBasil)], 
+                            Method = "BASIL")
+df_plier_cplot = data.frame(Observed = corP[lower.tri(corP)], 
+                            Predicted = corrPLIER[lower.tri(corrPLIER)],
+                            Method = "PLIER")
+lims = range(c(df_basil_cplot$Observed, df_basil_cplot$Predicted,
+               df_plier_cplot$Observed, df_plier_cplot$Predicted))
+
+p1_obs = plot_correlation_scatter(df_basil_cplot, title = "BASIL", lims)
+p2_obs = plot_correlation_scatter(df_plier_cplot, title = "PLIER", lims)
+
+obspPLot = grid.arrange(p1_obs, p2_obs, nrow=1)
+# ggsave('ObsPFever.png', plot=obspPLot, device = 'png', width = 8, height = 4)
+
