f98c8d3211de1f96597fd5809f5066b67a5e0fe1
federicastolf <federica.stolf@gmail.com>
2025-11-05 15:00:52 -0500

    update Fever

--- GFeverAnalysis.R
+++ GFeverAnalysis.R
@@ -8,15 +8,8 @@ library(tidyr)
 
 rm(list=ls())
 
-load("data_fever/data_Gfever.Rdata")
+# load("data_Gfever.Rdata")
 source("helper.R")
-sc = colSums(geneSetMat)
-pr1 = which(sc<11)
-geneSetMat = geneSetMat[,-pr1]
-sr = rowSums(geneSetMat)
-gr1 = which(sr==0)
-geneSetMat = geneSetMat[-gr1,]
-data = data[-gr1,]
 
 Y = t(data)
 Y = scale(Y)
@@ -30,9 +23,9 @@ Lambda_hat = fitGF$Lambda_C + fitGF$Lambda_N
 #------------------------------------------------------------------------------#
 #----------------------# correlation plot (Fig. 1) #---------------------------#
 
-# select the 100 genes with largest variance
-gene_variances <- apply(Y, 2, var)
-id <- order(gene_variances, decreasing = TRUE)[1:100]
+# subset 100 genes
+set.seed(134)
+id <- sample(c(1:dim(Y)[2]), 100)
 sub_data <- Y[,id]
 
 # empirical correlation
@@ -69,7 +62,8 @@ plier_corr_plot = plot_correlation_heatmap(corrPLIER, title = "PLIER", row_order
   col_order = col_order, breaks = breaks)
 corrNP = grid.arrange(emp_corr_plot$gtable, basil_corr_plot$gtable,
                                plier_corr_plot$gtable, ncol = 3)
-# ggsave('CorrFever.png', plot=CorrNP, device = 'png', width = 9.1, height = 3.3)
+
+# ggsave('CorrFever.png', plot=corrNP, device = 'png', width = 9.1, height = 3.3)
 
 #------# observed vs predicted plot #--------#
 
@@ -89,13 +83,14 @@ obspPLot = grid.arrange(p1_obs, p2_obs, nrow=1)
 # ggsave('ObsPFever.png', plot=obspPLot, device = 'png', width = 8, height = 4)
 
 
+
 #------------------------------------------------------------------------------#
 #----------------------------------# UQ for BASIL #----------------------------#
 
 # UQ for Lambda
 basil_loadings_samples = compute_posterior_samples_cc(
   Y, fitGF$Lambda_C, fitGF$Lambda_N, fitGF$tau_gamma, fitGF$tau_psi,
-  fitGF$sigma_sq, fitGF$P_C, v0=1, sigma_sq_0=1, n_MC=200)
+  fitGF$sigma_sq, fitGF$P_C, v0=1, sigma_sq_0=1, n_MC=500)
 
 # posterior samples of Gamma
 Gamma_samples = compute_Gamma_samples(basil_loadings_samples$Lambda_samples,
@@ -111,138 +106,83 @@ Gamma_UQ[contains_zero] = 0
 
 # get top pathways
 p_names = colnames(geneSetMat)
-data_pplot = get_topPathways(Gamma_UQ[,1:10], p_names, top_n = 10, clean_names = TRUE)
-
-# # rename labels too long
-# lab_pth = data_pplot$Pathway
-# lab_pth = sub(" ACTIVITY$", "", lab_pth)
-# long_indices = which(nchar(lab_pth) > 30)
-# lab_pth[long_indices]
-# pathways_short = c(
-#   "ABC GLUTATHIONE TRANSPORTER",              # 1
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 2
-#   "ATP INORGANIC ANION TRANS.",               # 3
-#   "NUCLEOSIDE TRANSPORTER",                   # 4
-#   "NUCLEOTIDE SUGAR TRANSPORTER",             # 5
-#   "PYRIMIDINE NUCLEOSIDE TRANS.",             # 6
-#   "ORGANOPHOSPHATE ANTIPORTER",               # 7
-#   "L-GLUTAMATE TRANSPORTER",                  # 8
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 9
-#   "PYRIMIDINE NUCLEOSIDE TRANS.",             # 10
-#   "L-GLUTAMATE TRANSPORTER",                  # 11
-#   "ORGANOPHOSPHATE ANTIPORTER",               # 12
-#   "ABC GLUTATHIONE TRANSPORTER",              # 13
-#   "L-SERINE TRANSPORTER",                     # 14
-#   "L-AMINO ACID TRANSPORTER",                 # 15
-#   "L-LEUCINE TRANSPORTER",                    # 16
-#   "NUCLEOSIDE TRANSPORTER",                   # 17
-#   "VEGFR2 BINDING",                           # 18
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 19
-#   "VEGFR BINDING",                            # 20
-#   "GABA-NA-CL SYMPORTER",                     # 21
-#   "MONOATOMIC ANION TRANSPORTER",             # 22
-#   "CHLORIDE TRANSPORTER",                     # 23
-#   "ABC GLUTATHIONE TRANSPORTER",              # 24
-#   "ATP INORGANIC ANION TRANS.",               # 25
-#   "MONOATOMIC ANION TRANSPORTER",             # 26
-#   "CHLORIDE TRANSPORTER",                     # 27
-#   "HEME OXIDOREDUCTASE",                      # 28
-#   "RETINOL DEHYDROG. (NADP+)",                # 29
-#   "VEGFR BINDING",                            # 30
-#   "VEGFR2 BINDING",                           # 31
-#   "ATP CATION TRANSPORTER",                   # 32
-#   "ACIDIC AMINO ACID TRANSPORTER",            # 33
-#   "INTEGRIN BINDING (CELL-ECM)",              # 34
-#   "PYRIMIDINE NUCLEOSIDE TRANS.",             # 35
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 36
-#   "NEUTRAL L-AA NA+ SYMPORTER",               # 37
-#   "ATP INORGANIC ANION TRANS.",               # 38
-#   "GLUTAMATE-GATED CA2+ CHANNEL",             # 39
-#   "L-GLUTAMATE TRANSPORTER",                  # 40
-#   "NUCLEOTIDE SUGAR TRANSPORTER",             # 41
-#   "L-PROLINE TRANSPORTER",                    # 42
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 43
-#   "NUCLEOSIDE TRANSPORTER",                   # 44
-#   "ABC GLUTATHIONE TRANSPORTER",              # 45
-#   "ATP INORGANIC ANION TRANS.",               # 46
-#   "NUCLEOTIDE SUGAR TRANSPORTER",             # 47
-#   "ORGANOPHOSPHATE ANTIPORTER",               # 48
-#   "ORGANIC HYDROXY TRANSPORTER",              # 49
-#   "CARBOHYDRATE DERIV. TRANS.",               # 50
-#   "L-GLUTAMATE TRANSPORTER",                  # 51
-#   "POLYOL TRANSPORTER",                       # 52
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 53
-#   "ORGANOPHOSPHATE ESTER TRANS.",             # 54
-#   "NUCLEOSIDE TRANSPORTER",                   # 55
-#   "NUCLEOTIDE SUGAR TRANSPORTER",             # 56
-#   "NUCLEOTIDE TRANSPORTER",                   # 57
-#   "ABC GLUTATHIONE TRANSPORTER",              # 58
-#   "ATP INORGANIC ANION TRANS.",               # 59
-#   "CARBOHYDRATE DERIV. TRANS.",               # 60
-#   "DEHYDROASCORBIC ACID TRANS.",              # 61
-#   "PYRIMIDINE NUCLEOSIDE TRANS.",             # 62
-#   "NUCLEOSIDE TRANSPORTER",                   # 63
-#   "L-GLUTAMATE TRANSPORTER",                  # 64
-#   "ABC GLUTATHIONE TRANSPORTER",              # 65
-#   "PURINE NUCLEOTIDE TRANSPORTER",            # 66
-#   "RETINOL DEHYDROG. (NADP+)",                # 67
-#   "ATP INORGANIC ANION TRANS.",               # 68
-#   "NUCLEOTIDE SUGAR TRANSPORTER",             # 69
-#   "ORGANOPHOSPHATE ESTER TRANS.",             # 70
-#   "ABC GLUTATHIONE TRANSPORTER",              # 71
-#   "L-GLUTAMATE TRANSPORTER",                  # 72
-#   "PYRIMIDINE NUCLEOSIDE TRANS.",             # 73
-#   "NUCLEOSIDE TRANSPORTER",                   # 74
-#   "ATP INORGANIC ANION TRANS.",               # 75
-#   "ORGANOPHOSPHATE ANTIPORTER",               # 76
-#   "NUCLEOTIDE SUGAR TRANSPORTER"              # 77
-# )
-# lab_pth[long_indices] = pathways_short
-# max(nchar(lab_pth))
-# data_pplot$Pathway = lab_pth
+data_pplot = get_topPathways(Gamma_UQ[,1:10], p_names, top_n = 5, 
+                             clean_names = TRUE, ci_lower = ci_lower[,1:10],
+                             ci_upper = ci_upper[,1:10])
 
 #--------# dotplot factor annontation #-----------#
+lab_pth = data_pplot$Pathway
+lab_pth = sub(" ACTIVITY$", "", lab_pth)
+long_indices <- which(nchar(lab_pth) > 30)
+lab_pth[long_indices]
+shortened_names  <- c(
+  "PI(3,5)P2 3-PHOSPHATASE",         # 1
+  "PI BISPHOSPHATE PHOSPHATASE",     # 2
+  "PI PHOSPHATE PHOSPHATASE",        # 3
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 4
+  "PI(3,5)P2 5-PHOSPHATASE",         # 5
+  "MACROMOLECULE TRANSPORTER",       # 6
+  "ABC GLUTATHIONE TRANSPORTER",     # 7
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 8
+  "PI(3,5)P2 3-PHOSPHATASE",         # 9
+  "PROTEIN TRANSPORTER",             # 10
+  "POSTSYNAPTIC NT RECEPTOR",        # 11
+  "DNA DAMAGE SENSOR",               # 12
+  "BETA-2 ADRENERGIC BINDING",       # 13
+  "ANION ATPASE",                    # 14
+  "ABC GLUTATHIONE TRANSPORTER",     # 15
+  "CATION ATPASE",                   # 16
+  "CALCIUM ANTIPORTER",              # 17
+  "PI PHOSPHATE PHOSPHATASE",        # 18
+  "PI(3,5)P2 3-PHOSPHATASE",         # 19
+  "NAD(P)H HEME OXIDOREDUCTASE",     # 20
+  "PYRIMIDINE TRANSPORT",            # 21
+  "PI(3,5)P2 3-PHOSPHATASE",         # 22
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 23
+  "PI PHOSPHATE PHOSPHATASE",        # 24
+  "PI(3,5)P2 PHOSPHATASE",           # 25
+  "PKA REGULATOR",                   # 26
+  "PI(3,5)P2 3-PHOSPHATASE",         # 27
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 28
+  "PI PHOSPHATE PHOSPHATASE",        # 29
+  "PI(3,5)P2 5-PHOSPHATASE",         # 30
+  "CATION ATPASE",                   # 31
+  "ABC GLUTATHIONE TRANSPORTER",     # 32
+  "PI(3,5)P2 3-PHOSPHATASE",         # 33
+  "PI(3,5)P2 PHOSPHATASE",           # 34
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 35
+  "MACROMOLECULE TRANSPORTER",       # 36
+  "PI(3,5)P2 5-PHOSPHATASE",         # 37
+  "PI(3,5)P2 3-PHOSPHATASE",         # 38
+  "POSTSYNAPTIC NT RECEPTOR",        # 39
+  "PI MONOPHOSPHATE PHOSPHATASE",    # 40
+  "PI PHOSPHATE PHOSPHATASE",        # 41
+  "PI(4,5)P2 5-PHOSPHATASE"          # 42
+)
+  
+lab_pth[long_indices] = shortened_names
+max(nchar(lab_pth))
+data_pplot$Pathway = lab_pth
+
 fplot = dotplot_top_pathways(data_pplot)
 fplot
-# ggsave('Factor_annotGF.png', plot=fplot, device = 'png', width = 9.1, height = 9)
-
-# posterior samples of Psi
-Psi_samples = compute_Psi_samples(basil_loadings_samples$Lambda_samples, fitGF$P_C)
-
-# 95% credible intervals
-ci_lowerpsi = apply(Psi_samples, c(1, 2), quantile, probs = 0.025)
-ci_upperpsi = apply(Psi_samples, c(1, 2), quantile, probs = 0.975)
-contains_zeropsi = (ci_lowerpsi < 0) & (ci_upperpsi > 0)
-table(contains_zeropsi)/length(contains_zeropsi) 
-Psi_UQ = apply(Psi_samples, c(1, 2), mean)
-Psi_UQ[contains_zero] = 0
-
-normPsi = rep(0, nrow(Psi_UQ))
-normPsi = apply(Psi_UQ, 1, function(x) sqrt(sum(x^2)))
-top_Psi = order(normPsi, decreasing = TRUE)[1:20]
-genePsi = rownames(geneSetMat[top_Psi,])
-genePsi
-
+# ggsave('Factor_annotGF_new.png', plot=fplot, device = 'png', width = 9.1, height = 9)
 
 
 #------------------------------------------------------------------------------#
 #----------------------------------# Bar plot #--------------------------------#
 
-c_sq = rowSums(fitGF$Lambda_C^2)[id]
-p_sq = rowSums(fitGF$Lambda_N^2)[id]
+gene_variances <- apply(t(data), 2, var)
+id50 <- order(gene_variances, decreasing = TRUE)[1:50]
+sub_data <- Y[,id50]
 
-gene_labs = colnames(Y)[id]
+c_sq = rowSums(fitGF$Lambda_C^2)[id50]
+p_sq = rowSums(fitGF$Lambda_N^2)[id50]
+gene_labs = colnames(Y)[id50]
 
 
-df = tibble(
-  i = seq_along(c_sq),
-  gene = gene_labs,
-  CGamma = c_sq,
-  Psi    = p_sq
-) %>%
-  mutate(total = CGamma + Psi,
-         CGamma = CGamma / total,
-         Psi    = Psi / total)  %>%
+df = tibble(i = seq_along(c_sq), gene = gene_labs, CGamma = c_sq, Psi = p_sq) %>%
+  mutate(total = CGamma + Psi, CGamma = CGamma / total, Psi    = Psi / total)  %>%
   pivot_longer(cols = c(CGamma, Psi), names_to = "component", values_to = "prop")
 
 df = df %>%
@@ -272,10 +212,12 @@ var_explained_plot <- ggplot(df, aes(x = gene, y = prop, fill = component)) +
   theme(
     panel.grid = element_blank(),
     axis.text.x = element_text(size = 9, angle = 45, hjust = 1, vjust = 1),
-    legend.text = element_text(size = 25),
+    legend.text = element_text(size = 30),
     plot.margin = margin(8, 10, 5, 20),
     axis.text.y = element_text(size = 15)
   )
 
 var_explained_plot
-#ggsave('var_explained_plot.png', plot=var_explained_plot, device = 'png', width = 15, height = 7.5)
+
+# ggsave('var_explained_plot50.png', plot=var_explained_plot, device = 'png', 
+#        width = 15, height = 7.5)
---
+++ getGFdata.R
@@ -0,0 +1,52 @@
+
+library(tidyverse)
+library(limma)
+library(edgeR)
+library(openxlsx)
+library(readxl)
+library(data.table)
+library(org.Hs.eg.db)
+library(msigdbr)
+library(AnnotationDbi)
+
+
+#--------------------------------# Load Data #---------------------------------#
+data <- read.table(file = 'GSE211567_normData_discovery_2021MAR24.txt', 
+                   sep = '\t', row.names = 1, header = TRUE)
+data[1:5, 1:5]
+
+#----------------------# Construct Prior Knowledge Matrix #--------------------#
+
+# map between Ensembl IDs and RefSeq IDs
+map <- AnnotationDbi::select(org.Hs.eg.db, key = row.names(data), columns = c("ENSEMBL"),
+    keytype = "REFSEQ")
+map <- map[is.na(map$ENSEMBL) == FALSE, ]
+length(unique(map$ENSEMBL))
+
+# get gene set information
+GOSet <- msigdbr(species = 'Homo sapiens', collection = 'C5', subcollection = 'GO:MF')
+head(GOSet)
+GOSet <- merge(GOSet, map, by.x = 'ensembl_gene', by.y = 'ENSEMBL', sort = FALSE)
+
+# construct prior knowledge matrix
+lcpm <- data
+geneCats <- unique(GOSet$gs_name)
+geneSetMat <- matrix(nrow = nrow(lcpm), ncol = length(geneCats))
+colnames(geneSetMat) <- geneCats
+row.names(geneSetMat) <- row.names(lcpm)
+
+for(j in 1:ncol(geneSetMat)){
+  geneSetMat[ ,j] <- as.numeric(row.names(geneSetMat) %in% 
+                                  GOSet$REFSEQ[GOSet$gs_name == colnames(geneSetMat)[j]])
+  if((j %% 100)==0){print(paste0('Iteration ', j, ' complete'))}
+}
+
+# filtered out pathways with fewer than 10 genes and genes that were not 
+# annotated to any gene set
+sc = colSums(geneSetMat)
+pr1 = which(sc<11)
+geneSetMat = geneSetMat[,-pr1]
+sr = rowSums(geneSetMat)
+gr1 = which(sr==0)
+geneSetMat = geneSetMat[-gr1,]
+data = data[-gr1,]
